1fdb710c6bfdde0cc0dc4ef84bf6901df0a2ac30
Oskar Lech <oskar.lech@oke.pl>
2016-03-23 17:14:12 +0100

    qwerty

---
+++ Solid Principles/Single Resposibility/Laugh.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Solid_Principles.Single_Resposibility
+{
+    class Laugh
+    {
+        private int _nrOfTimes; 
+        private string _laughSound;
+
+        public Laugh(string laughSound)
+        {
+            _laughSound = laughSound;
+        }
+
+        public string GenerateLaugh()
+        {
+            return string.Concat(Enumerable.Repeat(_laughSound, _nrOfTimes));
+        }
+
+
+
+    }
+}
