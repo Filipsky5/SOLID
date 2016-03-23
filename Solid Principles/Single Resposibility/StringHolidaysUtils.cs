45a00d0be4b27d1882aa440acc2c13397527d4d0
Marcin Olek <marcin@oke.pl>
2016-03-23 16:39:53 +0100

    single responsibility

--- Solid Principles/Single Resposibility/StringHolidaysUtils.cs
+++ Solid Principles/Single Resposibility/StringHolidaysUtils.cs
@@ -7,6 +7,31 @@ using System.Threading.Tasks;
 
 namespace CleanCode.SolidPrinciples
 {
+    
+    class LaughGenerator
+    {
+        private int numberOfTimes { private get }
+        private string laughSound { private get }
+        public LaughGenerator(int numberOfTimes, string laughSound) {
+            this.laughSound = numberOfTimes;
+            this.laughSound = laughSound;
+        }
+        
+        public generate() {
+            return string.Concat(Enumerable.Repeat(this.laughSound, this.numberOfTimes));
+        }    
+    }
+    
+    class HolidayCounter {
+        private string[] references { private get; }
+        public HolidayCounter(string[] references) {
+            this.references = references;
+        }
+        public int count(string holidayName) {
+            return this.references.Sum(r => Regex.Matches(holidayName.ToLower(), Regex.Escape(r.ToLower())).Count);
+        }
+    }
+    
     class StringHolidaysUtils
     {
         private string _laughSound;
